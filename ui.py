f94de1d899b8ba483a12aea3b9ae059f563e1374
vansh-9878 <aroravansh0501@gmail.com>
2026-03-24 15:29:47 +0530

    cleaned some code

--- ui.py
+++ ui.py
@@ -8,12 +8,12 @@ import speech_recognition as sr
 import azure.cognitiveservices.speech as speechsdk
 from dotenv import load_dotenv
 from agent import getAgent
+from voice import speak
 
 load_dotenv()
 
 WAKE_WORDS = ["hey assistant", "ok siri","hi jarvis"]
-SPEECH_KEY = os.getenv("SPEECH_KEY")
-SPEECH_REGION = os.getenv("SPEECH_REGION")
+
 
 BG_DARK    = "#0d0f14"
 BG_PANEL   = "#13161d"
@@ -30,37 +30,6 @@ AI_BUBBLE   = "#1e2535"
 SUCCESS    = "#22d3a0"
 DANGER     = "#f43f5e"
 WARN       = "#f59e0b"
-
-# def speak(text):
-#     try:
-#         cfg = speechsdk.SpeechConfig(subscription=SPEECH_KEY, region=SPEECH_REGION)
-#         cfg.speech_synthesis_voice_name = "en-US-CoraMultilingualNeural"
-#         synth = speechsdk.SpeechSynthesizer(speech_config=cfg)
-#         synth.speak_text_async(text).get()
-#     except Exception as e:
-#         print(f"TTS error: {e}")
-
-def speak(text, rate="+15%"):
-    try:
-        cfg = speechsdk.SpeechConfig(subscription=SPEECH_KEY, region=SPEECH_REGION)
-        cfg.speech_synthesis_voice_name = "en-US-CoraMultilingualNeural"
-        synth = speechsdk.SpeechSynthesizer(speech_config=cfg)
-
-        ssml = f"""
-        <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'>
-            <voice name='en-US-CoraMultilingualNeural'>
-                <prosody rate='{rate}'>
-                    {text}
-                </prosody>
-            </voice>
-        </speak>
-        """
-
-        result = synth.speak_ssml_async(ssml).get()
-        if result.reason == speechsdk.ResultReason.Canceled:
-            print(f"TTS canceled: {result.cancellation_details.reason}")
-    except Exception as e:
-        print(f"TTS error: {e}")
 
 class AssistantApp:
     def __init__(self):
